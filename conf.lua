c0bbe8c4dbd9759b93d65dd4fcc8e7f9fe081a2a
hanabyu <cherrybyuu@gmail.com>
2023-02-18 13:26:14 -0500

    start

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.console = true
+    t.window.title = "Escape From the Underworld"
+    t.window.vsync = 0
+    t.window.width = 960
+    t.window.height = 640
+end
---
+++ main.lua
@@ -0,0 +1,12 @@
+local bump = require "bump"
+local sti = require "sti"
+local classic = require "classic"
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
